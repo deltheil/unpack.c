2fcc7ebf425238b4fb2926fe13f98d4fe30b4aba
Cédric Deltheil <cedric@moodstocks.com>
2013-03-16 22:37:17 +0100

    stream input file

--- unpack.c
+++ unpack.c
@@ -1,50 +1,53 @@
 #include <stdio.h>
 #include <fcntl.h>    /* open */
-#include <sys/stat.h> /* fstat */
 #include <unistd.h>   /* read */
 #include <msgpack.h>
+
+/** defaults */
+#define CHK_SIZE    8192 /* read chunk size */
+#define BUF_SIZE   65536 /* streaming buffer init size */
 
 const char *g_cmd;
 const char *g_path = "";
 
 static void usage(void);
-static char *read_stdin(int *size);
-static char *read_file(const char *path, int *size);
 
 int main(int argc, char **argv) {
-  if (argc > 2) usage();
   g_cmd = argv[0];
+  if (argc > 2) usage();
   if (argc == 2) g_path = argv[1];
 
-  int size;
-  char *buf;
+  char buf[CHK_SIZE];
+  int fd;
+  ssize_t rsize;
   msgpack_unpacker u;
   msgpack_unpacked obj;
 
   if (!strcmp(g_path, "-"))
-    buf = read_stdin(&size);
+    fd = 0;
   else if (strlen(g_path) > 0)
-    buf = read_file(g_path, &size);
+    fd = open(g_path, O_RDONLY);
   else
-    buf = read_stdin(&size);
+    fd = 0;
 
-  if (!buf) goto readerr;
+  if (fd < 0) goto readerr;
 
-  msgpack_unpacker_init(&u, size);
+  msgpack_unpacker_init(&u, BUF_SIZE);
   msgpack_unpacked_init(&obj);
 
-  msgpack_unpacker_reserve_buffer(&u, size);
-  memcpy(msgpack_unpacker_buffer(&u), buf, size);
-  msgpack_unpacker_buffer_consumed(&u, size);
-
-  while (msgpack_unpacker_next(&u, &obj)) {
-    msgpack_object_print(stdout, obj.data);
-    putchar('\n');
+  while ((rsize = read(fd, buf, CHK_SIZE)) > 0) {
+    msgpack_unpacker_reserve_buffer(&u, rsize);
+    memcpy(msgpack_unpacker_buffer(&u), buf, rsize);
+    msgpack_unpacker_buffer_consumed(&u, rsize);
+    while (msgpack_unpacker_next(&u, &obj)) {
+      msgpack_object_print(stdout, obj.data);
+      putchar('\n');
+    }
   }
 
   msgpack_unpacker_destroy(&u);
   msgpack_unpacked_destroy(&obj);
-  free(buf);
+  if (fd > 0) close(fd);
 
   return 0;
 
@@ -56,36 +59,4 @@ readerr:
 static void usage(void) {
   fprintf(stderr, "usage: %s [file]\n", g_cmd);
   exit(1);
-}
-
-static char *read_stdin(int *size) {
-  msgpack_sbuffer sbuf;
-  msgpack_sbuffer_init(&sbuf);
-  char buf[8192];
-  int rsize;
-  while ((rsize = read(0, buf, sizeof(buf))) > 0)
-    msgpack_sbuffer_write(&sbuf, buf, rsize);
-  *size = sbuf.size;
-  return sbuf.data;
-}
-
-static char *read_file(const char *path, int *size) {
-  if (!path) return read_stdin(size);
-  int fd, len, rsize;
-  char *buf = NULL, *wp;
-  fd = open(path, O_RDONLY);
-  if (fd < 0) goto done;
-  struct stat sbuf;
-  if (fstat(fd, &sbuf) == -1) goto done;
-  len = sbuf.st_size;
-  buf = malloc(len);
-  wp = buf;
-  while ((rsize = read(fd, wp, len)) > 0) {
-    wp += rsize;
-    len -= rsize;
-  }
-  *size = sbuf.st_size;
-done:
-  if (fd > 0) close(fd);
-  return buf;
 }
